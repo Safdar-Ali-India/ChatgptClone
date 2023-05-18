696a3d7b9a35edaac0340ec814641bb688e67b42
Payfinix-safdar <safdar@payfinix.in>
2023-05-18 15:23:37 +0530

    added head and divs clone

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.html"],
+  theme: {
+    extend: {
+
+
+      colors:{
+        chatblack: {50: '#343541'},
+        // --color-secondary: rgb(111 114 185);
+      }
+    },
+  },
+  plugins: [],
+}
+
