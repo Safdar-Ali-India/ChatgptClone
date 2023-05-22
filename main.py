e9befea6b91b8ea2e56fed2f6a0a7d0e8b57c31d
Payfinix-safdar <safdar@payfinix.in>
2023-05-22 10:50:08 +0530

    new onsubmit ui changed

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template("index.html")
+
+@app.route('/api', methods=("GET","POST"))
+def qa():
+    if request.method == "POST":
+        data = {"result":"I'm glad to hear that you find ChatGPT helpful! As an AI language model, my purpose is to assist and provide information to the best of my abilities. If you have any questions or need assistance with anything specific, feel free to ask, and I'll do my best to help you."}
+        return jsonify(data)
+    data = {"result":"I'm glad to hear that you find ChatGPT helpful! As an AI language model, my purpose is to assist and provide information to the best of my abilities. If you have any questions or need assistance with anything specific, feel free to ask, and I'll do my best to help you."}
+    
+    return jsonify(data)
 
 app.run(debug =True)
